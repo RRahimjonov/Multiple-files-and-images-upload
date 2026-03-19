6260c5c7df6d15f6b90a0094a8ff71a29c7a924a
RRahimjonov <temurbekrahimjonov1210@gmail.com>
2026-03-19 20:18:17 +0500

    Bir necha rasmlarni saqlash algortmini qo'shdim

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreImageRequest;
+
+
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
 
 class ImageController extends Controller
 {
@@ -12,6 +16,27 @@ class ImageController extends Controller
     }
 
     public function store(StoreImageRequest $request){
-        dd('data stores');
+
+        $folder = "images/" . date('Y/m');
+
+        foreach ($request->images as $imageFile){
+
+            $filename = $this->makeUniqueFileName($imageFile);
+
+            $imageFile->storeAs($folder, $filename);
+        }
+    }
+
+    protected function makeUniqueFileName(UploadedFile $imageFile){
+
+        $originalName = $imageFile->getClientOriginalName();
+
+        $info = pathinfo($originalName);
+
+        return Str::slug($info['filename'])
+            . '_'
+            . time()
+            . '.'
+            . $info['extension'] ;
     }
 }
